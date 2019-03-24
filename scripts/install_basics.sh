97373e3a9d8521c11006560ff2f8a434e542cba2
apolloclark <apolloclark@gmail.com>
2019-03-24 11:53:33 -0400

    updated install_basics.sh

--- scripts/install_basics.sh
+++ scripts/install_basics.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eux
 
 if [ -x "$(command -v apt-get)" ]; then
-	apt-get update
-	apt-get upgrade -y
-    apt-get install -y aptitude python-minimal nano curl wget git apt-transport-https
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get upgrade -yq
+    apt-get install -yq aptitude python-minimal nano curl wget git apt-transport-https
 fi
 
 if [ -x "$(command -v yum)" ]; then
-	yum update -y
-	yum install -y sudo which nano curl wget git
+    yum update -y
+    yum install -y sudo which nano curl wget git
 fi
