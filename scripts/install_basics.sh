7ded10ebc0a949e97a1791cc2c27c6ac12f0a819
apolloclark <apolloclark@gmail.com>
2019-08-02 12:05:16 -0400

    fix gpg error

--- scripts/install_basics.sh
+++ scripts/install_basics.sh
@@ -13,16 +13,14 @@ elif [ -x "$(command -v dnf)" ]; then
         libtool make patch ruby sqlite-devel zlib-devel glibc-headers \
         glibc-devel openssl-devel # readline-dev bison
     dnf clean all
-    alternatives --set python /usr/bin/python3
-    python --version
 elif [ -x "$(command -v yum)" ]; then
     yum makecache fast
     yum update -y
     yum install -y which nano curl wget git gnupg2 initscripts hostname
     yum clean all
-    
-    # https://github.com/rvm/rvm/issues/4573
-    gpg --keyserver hkp://keys.gnupg.net \
-        --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
-        7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 fi
+
+# https://github.com/rvm/rvm/issues/4573
+gpg --keyserver hkp://keys.gnupg.net \
+    --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
+    7D2BAF1CF37B13E2069D6956105BD0E739499BDB
